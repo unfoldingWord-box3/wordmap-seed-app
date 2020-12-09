9ab18a52a2090d613c2b0f20e4a4c39d618c7fd6
Mike Brinker <themikejr@gmail.com>
2020-12-09 10:54:48 -0500

    Can render simple react div within svelte

---
+++ src/AlignmentEditorWrapper.svelte
@@ -0,0 +1,20 @@
+<script>
+	import * as React from 'react';
+	import * as ReactDOM from 'react-dom';
+    import { afterUpdate, onDestroy } from 'svelte';
+
+	let container;
+
+	afterUpdate(() => {
+		/*const { this: component, children, ...props } = $$props;*/
+		ReactDOM.render(
+            React.createElement('div', {}, 'Rendering this from react!'),
+			container
+		);
+	});
+	onDestroy(() => {
+		ReactDOM.unmountComponentAtNode(container);
+	});
+</script>
+
+<div bind:this={container} />
