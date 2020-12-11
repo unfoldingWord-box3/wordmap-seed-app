817ea90f6f563ed2f77e6cbcee2a3adebe55fa0b
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2020-12-11 18:01:00 +0100

    Keep un empty suggestion box in line with source

--- src/components/Suggestions.svelte
+++ src/components/Suggestions.svelte
@@ -24,7 +24,9 @@
         style="display: inline-block; border: solid; background-color: rgba(0,{(prediction.confidence >= 1) ? 255 : 0},{(prediction.confidence < 1) ? 255 : 0},{prediction.confidence}); padding: 0.2em;"
       >
         <div>{prediction.alignment.split('->')[0].replace(/:/g, ' ')}</div>
-        <div>{prediction.alignment.split('->')[1].replace(/:/g, ' ')}</div>
+        <div>
+          {@html prediction.alignment.split('->')[1].replace(/:/g, ' ') || '&nbsp;'}
+        </div>
       </div>
     {/each}
     </div>
