dc19a488f6b93134d16cf0ae4d09e755cfdbb45f
Mike Brinker <themikejr@gmail.com>
2020-12-14 10:02:24 -0500

    Buy more screen real estate.

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,32 @@
 <script>
-	import "smelte/src/tailwind.css";
-	import WordMap from './WordMap.svelte';
-	import Spreadsheet from './components/Spreadsheet.svelte';
-	import LanguageTextSelector from './components/LanguageTextSelector.svelte'
+  import "smelte/src/tailwind.css";
+  import WordMap from "./WordMap.svelte";
+  import Spreadsheet from "./components/Spreadsheet.svelte";
+  import LanguageTextSelector from "./components/LanguageTextSelector.svelte";
 </script>
 
-<main>
-	<h1>Hello WordMap!</h1>
-	<LanguageTextSelector />
-	<WordMap />
-	<Spreadsheet />
-</main>
-
 <style>
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  hr {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 
-	main {
-		padding-left: 1em;
-	}
+  main {
+    padding-left: 1em;
+  }
 </style>
+
+<main>
+  <h4>WordMAP Lab</h4>
+  <p>An interactive tool for evaluating wordMAP output.</p>
+  <p>Powered by Coffee™, backed by Google Sheets.</p>
+  <hr />
+  <LanguageTextSelector />
+  <WordMap />
+  <Spreadsheet />
+</main>
